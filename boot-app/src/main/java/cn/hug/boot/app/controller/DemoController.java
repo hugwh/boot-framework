ec837988969d99033018c41a8263ad64620e1c7a
huangwh <huangweihuang18@163.com>
2019-09-25 16:49:21 +0800

    配置logback

--- boot-app/src/main/java/cn/hug/boot/app/controller/DemoController.java
+++ boot-app/src/main/java/cn/hug/boot/app/controller/DemoController.java
@@ -2,6 +2,7 @@ package cn.hug.boot.app.controller;
 
 import cn.hug.boot.api.model.dto.result.TestDto;
 import cn.hug.boot.token.annotation.AuthToken;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import javax.validation.Valid;
  * @mail huangwh@txtws.com
  * @date: 2019-09-25 11:47
  */
+@Slf4j
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
@@ -22,5 +24,13 @@ public class DemoController {
     @GetMapping("/rest")
     public String rest(@RequestBody @Valid TestDto testDto) {
         return "rest";
+    }
+
+    @RequestMapping(value = "/index")
+    public String index() {
+        log.debug("记录debug日志");
+        log.info("记录index方法");
+        log.error("记录error日志");
+        return "index";
     }
 }
