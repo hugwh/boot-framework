f12db898e8234ba23c9ce85e61ea642cb4b52509
hug <hwh2coding@163.com>
2019-10-31 12:31:53 +0800

    pom优化

---
+++ boot-component/boot-wechat/src/main/java/cn/hug/boot/wechat/BootWechatApplication.java
@@ -0,0 +1,13 @@
+package cn.hug.boot.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootWechatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootWechatApplication.class, args);
+    }
+
+}
