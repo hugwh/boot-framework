cf4fc20fc263b87382ce835b807b63688591e911
hug <hwh2coding@163.com>
2019-11-03 01:48:59 +0800

    拦截器排除 微信服务认证接口 拦截

--- boot-app/src/main/java/cn/hug/boot/app/config/MvcInterceptorConfig.java
+++ boot-app/src/main/java/cn/hug/boot/app/config/MvcInterceptorConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
+ * 拦截器控制类
  * @author: huangwh
  * @date: 2019/11/3 0:22
  */
@@ -24,9 +25,16 @@ public class MvcInterceptorConfig extends WebMvcConfigurationSupport {
         // 多个拦截器组成一个拦截器链
         // addPathPatterns 用于添加拦截规则，/**表示拦截所有请求
         // excludePathPatterns 用户排除拦截
-        registry.addInterceptor(tokenAuthInterceptor).addPathPatterns("/**").order(-999);
+        registry.addInterceptor(tokenAuthInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/token/**")
+                .excludePathPatterns("/wechat/auth")
+                .order(-999);
 
-        registry.addInterceptor(signatureAuthInterceptor).addPathPatterns("/**").order(-998);
+        registry.addInterceptor(signatureAuthInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/wechat/auth")
+                .order(-998);
         super.addInterceptors(registry);
     }
 }
